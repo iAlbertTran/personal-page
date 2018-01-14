41b761857ef6bce929d142123b2bf47ba82418d3
Albert Tran <albtran@ucdavis.edu>
2018-01-14 03:02:31 -0800

    added a few functions with jquery code to animate the navbar appearance and effects

--- personalPage.js
+++ personalPage.js
@@ -1,10 +1,55 @@
+window.onload = function(){
+	navBarHover(true);
+}
+
 window.onscroll = function() {
-	var navBarContainer = document.getElementById("navBarContainer");
-	navBarContainer.style.backgroundColor = "#282828";
-	navBarContainer.style.color = "white";
-	
+
+	navBarColors(false);
+	navBarHover(false);
+
 	if($(window).scrollTop() === 0){
-		navBarContainer.style.backgroundColor = "white";
-		navBarContainer.style.color = "#282828";
+		navBarColors(true);
+		navBarHover(true);
 	}
+}
+
+//jquery code used to set the animations when hovering over anchors and logo in the navbar
+function navBarHover(topOfPage){
+	if(topOfPage){
+
+		var hoverAnimation = $("a, #aplusplus").mouseenter(function(){
+			$(this).css("background", "#282828").css("color", "white");
+		});
+
+
+		hoverAnimation.mouseleave(function(){
+			$(this).css("background", "white").css("color", "#282828");
+		});
+	}
+
+	else{
+
+		var hoverAnimation = $("a, #aplusplus").mouseenter(function(){
+			$(this).css("background", "white").css("color", "#282828");
+		});
+
+
+		hoverAnimation.mouseleave(function(){
+			$(this).css("background", "#282828").css("color", "white");
+		});
+	}
+}
+
+//changes the appearance of the navBar depending on if user scrolled all the way up or down
+//uses jquery to reduce code to two lines, rather than having to use a for loop on the anchors
+function navBarColors(topOfPage){
+	if(topOfPage){
+		$("a, #navBarContainer, #aplusplus").css("backgroundColor", "white").css("color", "#282828");
+		$("#aplusplus").css("borderColor", "#282828");
+	}
+	else{
+		$("a, #navBarContainer, #aplusplus").css("backgroundColor", "#282828").css("color", "white");
+		$("#aplusplus").css("borderColor", "white");
+	}
+
 }
